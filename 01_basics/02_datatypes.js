0b76ecaba1f0d96f78fb1f034e05976f9c7c453d
Pratham Sharma <144306825+prathamsharma10@users.noreply.github.com>
2025-03-28 13:48:56 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict" //treat all js code as newer version
+
+// alert("hello") // we are using node js not browser
+// ReferenceError: alert is not defined
+
+// console.log(3 + 3); console.log("pratham"); //bad practice
+
+// good code readability that should be high
+console.log("Pratham Sharma");
+console.log(3 + 3);
+
+let name = "Pratham"
+let age = 22
+let isLoggedIn = true
+let city; //typeof => undefined
+let state = null // typeof => object
+
+// number range =>  2 to power of 53
+// bigint
+// string => ""
+// boolean =>true or false
+// null => stand alone value 
+// undefined
+// symbol =>unique
+
+
+// object
+
+
+console.log(typeof "Pratham");
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof city);
+
+
+
+
+
+
