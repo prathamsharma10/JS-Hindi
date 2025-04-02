77e0595254b91038baf8daaab78cff6c1be69369
Pratham Sharma <144306825+prathamsharma10@users.noreply.github.com>
2025-04-02 12:59:06 +0000

    new

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(10 < 20);
+// console.log(10 > 20);
+// console.log(10 <= 20);
+// console.log(10 >= 20);
+// console.log(10 == 20);
+// console.log(10 != 20);
+
+// // console.log("2" > 1)
+
+// console.log(null < 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// console.log(null == 0);
+
+// === => strict check => checks data type also
+console.log("2" === 2); //false
