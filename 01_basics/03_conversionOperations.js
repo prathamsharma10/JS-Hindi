11bb9e0702fd8ce6b69bdc30841061872f326691
Pratham Sharma <144306825+prathamsharma10@users.noreply.github.com>
2025-04-02 11:47:45 +0000

    conversion and operation

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,124 @@
+// let score = 33
+// console.log(typeof score); //number
+// console.log(typeof(score));
+
+// let valueInString = String(score)
+// console.log(typeof valueInString); // converted to string
+// console.log(valueInString); //=> 33
+
+// let score = "33"
+// console.log(typeof score); //string
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // converted to number
+// console.log(valueInNumber); // =>33
+
+// let score = "33abc"
+// console.log(typeof score); //string
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // converted to number
+// console.log(valueInNumber); //=> NaN
+
+
+// let score = null
+// console.log(typeof score); //object
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // converted to number
+// console.log(valueInNumber); //=> 0
+
+// let score = undefined
+// console.log(typeof score); //undefined
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // converted to number
+// console.log(valueInNumber); //=> NaN
+
+// let score = true
+// console.log(typeof score); //boolean
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // converted to number
+// console.log(valueInNumber); //=> 1
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0   
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //true
+
+
+// let isLoggedIn = 0
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //false
+
+// let isLoggedIn = "pratham"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //true
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //false
+
+// 1 => true 
+// 0 => false
+// "" => false
+//"pratham" => true
+
+// Operations in js
+
+// let value  = 3 
+
+// let negValue = -(value)
+// console.log(negValue);//-3
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%2);
+
+// console.log(2**3); // 2**3 => 2^3 =? 2*2*2 => 8
+
+// let str1 ="hello"
+// let str2 = " world"
+
+// let str3 = (str1+str2)
+
+// console.log(str3);
+
+// if string is first priority is given to string and vice versa for numbers
+
+// console.log("1"+2); //12
+
+// console.log(1+"2"); //12
+
+// console.log(5+4+"2"); //12
+
+// console.log("1"+2+2); //122
+
+// console.log("1"+2+4); //124
+
+// console.log(1+2+"2"); //32
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+// let gameCounter  = 100
+// gameCounter++
+// console.log(gameCounter)
+
+
+
