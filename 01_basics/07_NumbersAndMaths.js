793898567b22b831fbdf89eaba5ad0b70f939388
Pratham Sharma <144306825+prathamsharma10@users.noreply.github.com>
2025-04-04 04:07:57 +0000

    number

---
+++ 01_basics/07_NumbersAndMaths.js
@@ -0,0 +1,42 @@
+// const score = 100
+// console.log(score);
+
+// const balance = new Number(400)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const newNumber = 125.967
+// console.log(newNumber.toPrecision(3));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN')); //10,000,000us, 
+// // 1,00,00,000IN
+
+// Maths in js
+
+// console.log(Math); //Object [Math] {}
+ 
+// console.log(Math.abs(-10)); //10
+// console.log(Math.round(4.3)); //4
+// console.log(Math.ceil(4.3)); //5
+// console.log(Math.floor(4.3)); //4
+// console.log(Math.min(1,2,3,45,6)); //1
+// console.log(Math.max(1,2,3,45,6)); //45
+
+console.log(Math.random()); // random value between 0 and 1
+console.log((Math.random()*10) +1); // assures no 0 will come
+console.log(Math.floor(Math.random()*10) +1); // 
+
+const min  = 10
+const max  = 20
+
+console.log(Math.floor(Math.random() *(max - min +1)) + min)
+
+
+
+
+
+
+
